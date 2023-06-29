02879704e209f2daf0a7d656ecd88beb5493d58b
Gustavo Monteiro <ggpm20@hotmail.com.br>
2023-06-29 15:01:39 -0300

    Scripts with animation numbers completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
-function setResults() {
-  const scoredElement = document.querySelector('.scored');
+function setAnimatedResult(elementQuery, value) {
+  const steps = 4;
   let number = 0;
+  const element = document.querySelector(elementQuery);
   const timer = setInterval(() => {
-    scoredElement.innerText = number;
-    number++;
-    if (number > 10 ) {
+    if (number > steps) {
       clearInterval(timer);
+      element.innerText = value;
+      return;
     }
-  }, 100);
+    element.innerText = Math.round((value / steps) * number);
+    number += 1;
+  }, 200);
 }
 
-setResults();
+const listElement = document.querySelector('.summary-items');
+
+function fillList(data) {
+  data.forEach((item) => {
+    const { category, score, icon } = item;
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `<img class="icon" src="${icon}" alt="">
+    <p class="name">${category}</p>
+    <p class="score"><span>${score}</span> / 100</p>`;
+    const categoryLowerCase = category.toLowerCase();
+    listItem.classList.add(categoryLowerCase);
+    listElement.appendChild(listItem);
+    setAnimatedResult(`li.${categoryLowerCase} .score span`, score);
+  });
+}
+
+fetch('../data.json')
+  .then((res) => res.json())
+  .then((json) => {
+    fillList(json);
+
+    let averageScore = json.reduce((accumulator, { score }) => accumulator + score, 0);
+    averageScore = Math.floor(averageScore / json.length);
+
+    setAnimatedResult('.circle .scored', averageScore);
+  });
